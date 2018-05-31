feb045f0528a944d0555c479268ec8fd3ca50a9e
AliReza Tofighi Mohammadi <alirtofighim@gmail.com>
2018-05-31 14:28:54 +0430

    add gitignore

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require("webpack");
+const bundlePath = path.resolve(__dirname, "dist/");
+
+module.exports = {
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: { presets: ['env'] }
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.(s?)css$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        publicPath: bundlePath,
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname,'public'),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist"
+    },
+    plugins: [ new webpack.HotModuleReplacementPlugin() ]
+};
